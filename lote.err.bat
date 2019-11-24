124d955b1de4edcfed51c70ceba7fd9a402d259f
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-24 12:13:32 -0500

    se agrego lote.err.bat

---
+++ lote.err.bat
@@ -0,0 +1,102 @@
+python Carrillo/validadores/validador01.py "pan" "hola"
+python Carrillo/validadores/validador02.py "estoy" "feliz"
+python Carrillo/validadores/validador03.py "tengo"
+python Carrillo/validadores/validador04.py "A" "B"
+python Carrillo/validadores/validador05.py "HOLA" "COMER"
+python Carrillo/validadores/validador06.py "TU" "MADRE"
+python Carrillo/validadores/validador07.py "NO" "SI"
+python Carrillo/validadores/validador08.py "ACEPTO" "NEL PRRO"
+python Carrillo/validadores/validador09.py "ADIOS" ":'V"
+python Carrillo/validadores/validador10.py "XD" "FREE"
+
+
+
+python Carrillo/interacion/interacion11.py A
+python Carrillo/interacion/interacion12.py V
+python Carrillo/interacion/interacion13.py S
+python Carrillo/interacion/interacion14.py H
+python Carrillo/interacion/interacion15.py J
+python Carrillo/interacion/interacion16.py JU
+python Carrillo/interacion/interacion17.py Q
+python Carrillo/interacion/interacion18.py J
+python Carrillo/interacion/interacion19.py D
+python Carrillo/interacion/interacion20.py FREE
+
+
+
+python Carrillo/bucles_para/bucle_para21.py 12
+python Carrillo/bucles_para/bucle_para22.py 343
+python Carrillo/bucles_para/bucle_para23.py 312
+python Carrillo/bucles_para/bucle_para24.py 87
+python Carrillo/bucles_para/bucle_para25.py 2
+python Carrillo/bucles_para/bucle_para26.py 2.4
+python Carrillo/bucles_para/bucle_para27.py 4.7
+python Carrillo/bucles_para/bucle_para28.py 12
+python Carrillo/bucles_para/bucle_para29.py 3.17
+python Carrillo/bucles_para/bucle_para30.py 98.5
+
+
+
+
+python Carrillo/decodificador/decodificador31.py 56
+python Carrillo/decodificador/decodificador32.py 34
+python Carrillo/decodificador/decodificador33.py 12
+python Carrillo/decodificador/decodificador34.py 56
+python Carrillo/decodificador/decodificador35.py 7
+python Carrillo/decodificador/decodificador36.py 2
+python Carrillo/decodificador/decodificador37.py 6
+python Carrillo/decodificador/decodificador38.py 12
+python Carrillo/decodificador/decodificador39.py 45
+python Carrillo/decodificador/decodificador40.py 67
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+python Damian / bucles_iteracion / decodificador1.py WXYZ
+python Damian / bucles_iteracion / decodificador2.py 1234
+python Damian / bucles_iteracion / decodificador3.py ABCD
+python Damian / bucles_iteracion / decodificador4.py 123456
+python Damian / bucles_iteracion / decodificador5.py 1234567891011
+
+python Damian / bucles_iteracion_rango / ejercicio1.py 20.4
+python Damian / bucles_iteracion_rango / ejercicio2.py 100.0
+python Damian / bucles_iteracion_rango / ejercicio3.py 10.1
+python Damian / bucles_iteracion_rango / ejercicio4.py 28.22
+python Damian / bucles_iteracion_rango / ejercicio5.py 80.2
+
+python Damian / bucles_mientras / validador1.py 58 9.8
+python Damian / bucles_mientras / validador2.py 150.5 200
+python Damian / bucles_mientras / validador3.py 24 32.5
+pitón Damian / bucles_mientras / validador4.py 150  105 35,5
+python Damian / bucles_mientras / validador5.py 7.5
+
+python Damian / bucles_para / recorrido1.py a
+python Damian / bucles_para / recorrido2.py 100 210.5
+python Damian / bucles_para / recorrido3.py 200 15.0
+pitón Damián / bucles_para / recorrido4.py 100 X
+python Damian / bucles_para / recorrido5.py W
