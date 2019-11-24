eab9a2948887903a8c629e9eb34f6f0614bfdb75
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-11-24 12:13:05 -0500

    se agrego lote.bat

---
+++ lote.bat
@@ -0,0 +1,111 @@
+python Carrillo/validadores/validador01.py 1.34 2.00
+python Carrillo/validadores/validador02.py 50.2 75.2
+python Carrillo/validadores/validador03.py 11.0 20
+python Carrillo/validadores/validador04.py 37.3 39.5
+python Carrillo/validadores/validador05.py 33.4 35.6
+python Carrillo/validadores/validador06.py 100 200
+python Carrillo/validadores/validador07.py 13 18
+python Carrillo/validadores/validador08.py 184 300
+python Carrillo/validadores/validador09.py 1.20 2.00
+python Carrillo/validadores/validador10.py 50 70
+
+
+
+python Carrillo/interacion/interacion11.py 200
+python Carrillo/interacion/interacion12.py 100
+python Carrillo/interacion/interacion13.py 200
+python Carrillo/interacion/interacion14.py 300
+python Carrillo/interacion/interacion15.py 200
+python Carrillo/interacion/interacion16.py 400
+python Carrillo/interacion/interacion17.py 500
+python Carrillo/interacion/interacion18.py 10
+python Carrillo/interacion/interacion19.py 45
+python Carrillo/interacion/interacion20.py 600
+
+
+
+python Carrillo/bucles_para/bucle_para21.py "hola"
+python Carrillo/bucles_para/bucle_para22.py "pedro ruiz gallo"
+python Carrillo/bucles_para/bucle_para23.py "mi madre es la mejor"
+python Carrillo/bucles_para/bucle_para24.py "te quiero"
+python Carrillo/bucles_para/bucle_para25.py "tucume"
+python Carrillo/bucles_para/bucle_para26.py "calle san juan"
+python Carrillo/bucles_para/bucle_para27.py "promocion JESMET"
+python Carrillo/bucles_para/bucle_para28.py "lenovo"
+python Carrillo/bucles_para/bucle_para29.py "solo te quiero"
+python Carrillo/bucles_para/bucle_para30.py "mi nombre es brayan"
+
+
+
+
+python Carrillo/decodificador/decodificador31.py "hola" "mucho gusto" "mi nombre es" "brayan"
+python Carrillo/decodificador/decodificador32.py "me podrias" "decir" "donde queda" "la casa" "de juan" "por favor"
+python Carrillo/decodificador/decodificador33.py "buen hombre" "rudo" "atletico" "y" "atento" "nunca :v"
+python Carrillo/decodificador/decodificador34.py "ni loco" "regreses con ella"
+python Carrillo/decodificador/decodificador35.py "alma" "noble" "toxica" "ociosa" "nunca es" "yoyo xd"
+python Carrillo/decodificador/decodificador36.py "mujer" "adorable" "de admirar" "romantica" "eterna"
+python Carrillo/decodificador/decodificador37.py "universidad" "nacional" "pedro" "ruiz" "gallo"
+python Carrillo/decodificador/decodificador38.py "facultad de" "ciencias" "fisicas" "y" "matematicas"
+python Carrillo/decodificador/decodificador39.py "estoy" "feliz de estar" "a tu lado"
+python Carrillo/decodificador/decodificador40.py "patria" "esfuerzo" "rifle" "union"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+python Damian / bucles_iteracion / decodificador1.py 1234
+python Damian / bucles_iteracion / decodificador2.py ABCD
+python Damian / bucles_iteracion / decodificador3.py 1345
+python Damian / bucles_iteracion / decodificador4.py PQSRT
+python Damian / bucles_iteracion / decodificador5.py PQRSTVWXYZ
+
+python Damian / bucles_iteracion_rango / ejercicio1.py 100
+python Damian / bucles_iteracion_rango / ejercicio2.py 25
+python Damian / bucles_iteracion_rango / ejercicio3.py 36
+python Damian / bucles_iteracion_rango / ejercicio4.py 18
+python Damian / bucles_iteracion_rango / ejercicio5.py 10  30
+
+python Damian / bucles_mientras / validador1.py 46  20
+pitón Damian / bucles_mientras / validador2.py 150  300
+python Damian / bucles_mientras / validador3.py 48  32
+pitón Damian / bucles_mientras / validador4.py 180  105  35
+python Damian / bucles_mientras / validador5.py 7
+
+python Damian / bucles_para / recorrido1.py 20
+python Damian / bucles_para / recorrido2.py 20  30
+pitón Damián / bucles_para / recorrido3.py 2  8
+python Damian / bucles_para / recorrido4.py 10  25
+python Damian / bucles_para / recorrido5.py 100
+
+
